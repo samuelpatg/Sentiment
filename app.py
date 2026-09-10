2bfed27af95c588f1e9f4737fbd914824b0f1a13
samuelpatg <samuelpatg@gmail.com>
2026-09-10 13:23:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from streamlit_lottie import st_lottie
 import json
 import streamlit as st
 from PIL import Image
-#from googletrans import Translator
+from googletrans import Translator
 
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
 st.image(image)
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 
-#translator = Translator()
+translator = Translator()
 
 with st.sidebar:
                st.subheader("Polaridad y Subjetividad")
